413ff2b4e0dde25720156c1e7446ac9a4e34418c
Gabrielly Da Costa <gabriellydacostaa@gmail.com>
2025-08-26 15:52:46 -0300

    Add files via upload

---
+++ aniversario.py
@@ -0,0 +1,4 @@
+dia = input("Digite o dia que você nasceu: ")
+mes = input("Em qual mês você nasceu? " )
+ano = input("Em qual ano você nasceu? ")
+print("Você nasceu no dia", dia, "do", mes, "de", ano, "Correto?")
---
+++ ex02.py
@@ -0,0 +1,6 @@
+ask = input("Qual é o seu nome: ")
+print("Prazer em te conhecer", ask)
+
+
+nome = input("Qual é o seu sobrenome? ")
+print("Prazer em te conhecer, sr. {}!".format(nome)) 

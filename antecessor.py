9ec467527d2e3706d562380d96ac028b78bfaa6a
GabriellydaCosta <gabriellydacostaa@gmail.com>
2025-10-06 11:58:13 -0300

    Antecessor e sucessor

---
+++ antecessor.py
@@ -0,0 +1,6 @@
+#5) programa que leia um número inteiro e mostre seu antecessor e sucessor
+
+numero = int(input("Digite um valor: "))
+antecessor = numero - 1
+sucessor = numero + 1
+print("O antecessor de {} é {}, e o sucessor é {}".format(numero, antecessor, sucessor))

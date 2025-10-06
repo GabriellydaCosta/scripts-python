f407f7c2b93625e9c2375b23ad731dff45a18d92
GabriellydaCosta <gabriellydacostaa@gmail.com>
2025-10-06 13:21:27 -0300

    média entre duas notas

---
+++ media.py
@@ -0,0 +1,6 @@
+#7) Ler duas notas de um aluno e calcular sua média
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+media = (nota1 + nota2)/2
+print("Sua média é {}!".format(media))

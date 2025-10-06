14398111a9b624fba445ed033691c733a3907478
GabriellydaCosta <gabriellydacostaa@gmail.com>
2025-10-06 13:34:13 -0300

    metro_centimetro.py

---
+++ metro_centimetro.py
@@ -0,0 +1,6 @@
+#8) ler o valor em metros e converter em centímetros e milímetros (fazer interface para esse)
+
+metro = float(input("Digite o valor em metros: "))
+cm = metro * 100
+mm = metro * 1000
+print("{} metros são {} cm e {} mm".format(metro, cm, mm))

493ce086c7d0332a47d17db3d01e081a8e374103
GabriellydaCosta <gabriellydacostaa@gmail.com>
2025-10-06 13:46:26 -0300

    Tabuada

---
+++ tabuada.py
@@ -0,0 +1,15 @@
+#9) ler um número inteiro qualquer e mostrar na tela sua tabuada
+num = int(input("Digite um número inteiro: "))
+
+um = num * 1
+dois = num * 2
+tres = num * 3
+quatro = num * 4
+cinco = num * 5
+seis = num * 6
+sete = num * 7
+oito = num * 8
+nove = num * 9
+dez = num * 10
+
+print("A tabuada de {} é: \n {} x 1 = {} \n {} x 2 = {} \n {} x 3 = {} \n {} x 4 = {} \n {} x 5 = {} \n {} x 6 = {} \n {} x 7 = {} \n {} x 8 = {} \n {} x 9 = {} \n {} x 10 = {} ".format(num, num, um, num, dois, num, tres, num , quatro, num, cinco, num, seis, num, sete, num, oito, num, nove, num, dez))

1b1a98a55a5cd4410084214f23f9aa4f1695d0cc
GabriellydaCosta <gabriellydacostaa@gmail.com>
2025-10-06 19:10:49 -0300

    Calculando a área de uma parede em metros e verificando quanto de tinta em litros precisa para pintá-la

--- quantidade_tinta.py
+++ quantidade_tinta.py
@@ -1,7 +1,8 @@
-#11) ler largura e altura de uma parede em metros e calcular area e quantidade de tinta para pintar. 1l pinta 2m² (fazer interface para esse)
+'''11) ler largura e altura de uma parede em metros e calcular area e quantidade de tinta para pintar. 1l pinta 15m²  de acordo
+com o site https://www.suvinil.com.br/blog/um-litro-de-tinta-pinta-quantos-metros-quadrados (fazer interface para esse)'''
 
 largura = float(input("Digite a largura da parede em metros: "))
 altura = float(input("Digite a altura da parede em metros: "))
 area = largura * altura
-qtd_tinta = area / 2
+qtd_tinta = area / 15
 print("A área total da parede é {:.1f}m², e a quantidade de tinta em litros necessária para pintar a parede é {:.1f}L".format(area, qtd_tinta))

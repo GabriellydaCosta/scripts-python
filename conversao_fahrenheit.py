f28abb0c486da7ed236d84a74b6ea219a67fdafe
GabriellydaCosta <gabriellydacostaa@gmail.com>
2025-10-09 21:04:08 -0300

    Conversor de graus celsius para fahrenheit

---
+++ conversao_fahrenheit.py
@@ -0,0 +1,5 @@
+#14) Conversão de graus de celsius para fahrenhit
+
+celsius = int(input("Digite a temperatura em graus celsius: "))
+fahrenheit = (celsius * 1.8) + 32
+print("{} graus celsius são {} graus fahrenheit".format(celsius, fahrenheit))
